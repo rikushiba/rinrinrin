e8ed2c1c07a11d6735b3b20573a381d51e566533
rinrinri <67157219+rinrinri@users.noreply.github.com>
2020-06-28 01:37:09 +0900

    ちょっとスクリプト書いたよ

--- Rensyuu2/Assets/KyotenBuildingLayerScript.cs
+++ Rensyuu2/Assets/KyotenBuildingLayerScript.cs
@@ -6,27 +6,35 @@ using UnityEngine.UIElements;
 
 public class KyotenBuildingLayerScript : MonoBehaviour
 {
-    void PositionGet()
+    public TileBase Kyotentile;
+    void Fill()
     {
-        int i = 0;
         var tilemap = GetComponent<Tilemap>();
-        Vector3Int[] position = new Vector3Int[400];
-        var bound = tilemap.cellBounds;
-        for (int y = bound.max.y-1;y>=bound.min.y; --y)
-        {
-            for (int x = bound.min.x; x<bound.max.x; ++x)
-            {
-                position[i] = new Vector3Int(x, y, 0);
-                i++;
-            }
-        }
-         
+        var position = new Vector3Int(0, 0, 0);
+        tilemap.BoxFill(position, Kyotentile, -10, -10, 10, 10);
     }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        void PositionGet()
+        {
+            int i = 0;
+            var tilemap = GetComponent<Tilemap>();
+            Vector3Int[] position = new Vector3Int[400];
+            var bound = tilemap.cellBounds;
+            for (int y = bound.max.y - 1; y >= bound.min.y; --y)
+            {
+                for (int x = bound.min.x; x < bound.max.x; ++x)
+                {
+                    position[i] = new Vector3Int(x, y, 0);
+                    i++;
+                }
+            }
+
+        }
     }
+    
 
     // Update is called once per frame
     void Update()
