af589083f4ab2bd57e1a4dc9f316d63a5d849beb
rinrinri <67157219+rinrinri@users.noreply.github.com>
2020-07-02 02:51:32 +0900

    no message

---
+++ Rensyuu2/Assets/UpbuttonScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpbuttonScript : MonoBehaviour
+{
+    public GameObject frame;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnMouseDown()
+    {
+        frame.transform.Translate(0, 1, 0);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        OnMouseDown();
+    }
+}
