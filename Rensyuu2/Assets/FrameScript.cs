60fd876b3227674059ecdf881e68a19923c8a87b
rikushiba <67112363+rikushiba@users.noreply.github.com>
2020-07-02 02:49:59 +0900

    FrameScriptでタップした位置にフレームが移動するようにしたよ

--- Rensyuu2/Assets/FrameScript.cs
+++ Rensyuu2/Assets/FrameScript.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class FrameScript : MonoBehaviour
 {
+    Vector3 Mposition;
+    Vector3 Obp;
+    Vector3 screenToWorldPointPosition;
+    GameObject frame;
+    Transform ObTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        frame = GameObject.Find("frame");
+        ObTransform = this.transform;
+        Obp = ObTransform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            Mposition = Input.mousePosition;
+            Mposition.z = 20f;
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(Mposition);
+            if (-14 <= screenToWorldPointPosition.x && screenToWorldPointPosition.x <= 7 && -10 <= screenToWorldPointPosition.y && screenToWorldPointPosition.y <= 11)
+            {
+                Obp.x = (Mathf.Ceil(screenToWorldPointPosition.x) + Mathf.Floor(screenToWorldPointPosition.x)) / 2;
+                Obp.y = (Mathf.Ceil(screenToWorldPointPosition.y) + Mathf.Floor(screenToWorldPointPosition.y)) / 2;
+                ObTransform.position = Obp;
+            }
+        }
     }
 }
