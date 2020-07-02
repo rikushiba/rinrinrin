09ee1a41ce9f764781532166740026dc6c0b2671
rinrinri <67157219+rinrinri@users.noreply.github.com>
2020-07-03 01:09:22 +0900

    海にもボタンスクリプトを書いたよ！確認してね！

---
+++ Rensyuu2/Assets/UmiDownbuttonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmiDownbuttonScript : MonoBehaviour
+{
+    GameObject frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frame = GameObject.Find("frame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onClickAct()
+    {
+        if (frame.transform.position.y > -8)
+        {
+            frame.transform.Translate(0, -1, 0);
+        }
+        else
+        {
+            frame.transform.Translate(0, 0, 0);
+        }
+    }
+}
---
+++ Rensyuu2/Assets/UmiLeftbuttonScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UmiLeftbuttonScript : MonoBehaviour
+{
+    GameObject frame;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frame = GameObject.Find("frame");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void onClickAct()
+    {
+        if (frame.transform.position.x > -13)
+        {
+            frame.transform.Translate(-1, 0, 0);
+        }
+        else
+        {
+            frame.transform.Translate(0, 0, 0);
+        }
+    }
+}
