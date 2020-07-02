4b4ab3605683f239cb281e9eafbb1f81625d0cce
rikushiba <67112363+rikushiba@users.noreply.github.com>
2020-07-03 00:58:35 +0900

    拠点シーンで拠点フレームスクリプトをかいたよ

---
+++ Rensyuu2/Assets/KyotenFrameScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KyotenFrameScript : MonoBehaviour
+{
+    Vector3 Mposition;
+    Vector3 Obp;
+    Vector3 screenToWorldPointPosition;
+    GameObject frame;
+    Transform ObTransform;
+    // Start is called before the first frame update
+    void Start()
+    {
+        frame = GameObject.Find("frame");
+        ObTransform = this.transform;
+        Obp = ObTransform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Mposition = Input.mousePosition;
+            Mposition.z = 20f;
+            screenToWorldPointPosition = Camera.main.ScreenToWorldPoint(Mposition);
+            if (-10 <= screenToWorldPointPosition.x && screenToWorldPointPosition.x <= 10 && -9 <= screenToWorldPointPosition.y && screenToWorldPointPosition.y <= 10)
+            {
+                Obp.x = (Mathf.Ceil(screenToWorldPointPosition.x) + Mathf.Floor(screenToWorldPointPosition.x)) / 2;
+                Obp.y = (Mathf.Ceil(screenToWorldPointPosition.y) + Mathf.Floor(screenToWorldPointPosition.y)) / 2;
+                ObTransform.position = Obp;
+            }
+        }
+    }
+}
