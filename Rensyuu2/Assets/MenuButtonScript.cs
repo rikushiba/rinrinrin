664b6209b4c29d18cd63d5b9d869d9c881a2006c
rikushiba <67112363+rikushiba@users.noreply.github.com>
2020-08-04 00:54:47 +0900

    no message

---
+++ Rensyuu2/Assets/MenuButtonScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+using UnityEngine;
+
+public class MenuButtonScript : MonoBehaviour
+{
+    public GameObject MenuText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+    }
+
+    public void onClickAct()
+    {
+        Debug.Log("a");
+        if (MenuText.active)
+        {
+            MenuText.SetActive(false);
+        }
+        else
+        {
+            MenuText.SetActive(true);
+        }
+    }
+}
