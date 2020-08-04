51bfd8794e084d2e17061dac5d69f28f060abbc0
rikushiba <67112363+rikushiba@users.noreply.github.com>
2020-08-04 15:24:47 +0900

    SEND SCRIPTを変更したよ！！！！！！！！！！！！！！！！！！！！！

--- Rensyuu2/Assets/SendScript.cs
+++ Rensyuu2/Assets/SendScript.cs
@@ -38,6 +38,8 @@ public class SendScript : MonoBehaviour
     public GameObject Close;
     public GameObject Select;
     public GameObject Menu;
+    Color SelectedTextColor = new Color(1.0f, 0.0f, 0.0f, 1.0f);
+    Color UnselectedTextColor = new Color(1.0f, 1.0f, 1.0f, 1.0f);
 
     // Start is called before the first frame update
     void Start()
--- Rensyuu2/Assets/UmiUpbuttonScript.cs
+++ Rensyuu2/Assets/UmiUpbuttonScript.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UmiUpbuttonScript : MonoBehaviour
 {
     GameObject frame;
+    GameObject[] MenuList = new GameObject[10] ;
+    Text[] Menu = new Text[10];
     public GameObject MenuText;
     // Start is called before the first frame update
     void Start()
-    {
+    { 
+        int i=0;
+        int Count=MenuText.transform.childCount;
         frame = GameObject.Find("frame");
+        while (Count > i)
+        {
+            MenuList[i] = MenuText.transform.GetChild(i).gameObject;
+            Menu[i] = MenuList[i].GetComponent<Text>();
+            i++;
+        }
     }
 
     // Update is called once per frame
