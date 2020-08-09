b9acb3d1f6b210ad3dba25cc844577698d5a7cc6
rikushiba <67112363+rikushiba@users.noreply.github.com>
2020-08-10 02:34:45 +0900

    いろいろ

--- Rensyuu2/Assets/KyotenFrameScript.cs
+++ Rensyuu2/Assets/KyotenFrameScript.cs
@@ -36,7 +36,7 @@ public class KyotenFrameScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (KT && Close && Frame2.activeSelf)
+        if (Close.activeSelf)
         {
 
         }
